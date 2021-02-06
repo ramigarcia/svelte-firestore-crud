ceaa97a049c7d4e10e765299a191fb57f4eb2cb1
Ramiro Garcia <rg2607836@gmail.com>
2021-02-06 06:11:20 -0300

    [Add] Configuracion Firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCVBiB9XL9FJSapVOSB3FuqZWmNqsYFHBk",
+  authDomain: "svelte-crud-e23c3.firebaseapp.com",
+  projectId: "svelte-crud-e23c3",
+  storageBucket: "svelte-crud-e23c3.appspot.com",
+  messagingSenderId: "157944923942",
+  appId: "1:157944923942:web:4056c1e34504486d7ca3e7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
