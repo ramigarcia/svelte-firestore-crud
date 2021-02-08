c2da57acb4cdc07084711de562cb413eeb06e51a
Ramiro Garcia <rg2607836@gmail.com>
2021-02-08 19:38:05 -0300

    [Add] estilos

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,65 @@
 <script>
-  const handleSubmit = (e) => {
-    e.preventDefault("");
+  const handleSubmit = () => {
     console.log("Guardado");
   };
 </script>
 
-<form on:submit={handleSubmit}>
-  <input type="text" placeholder="Escribe una tarea" />
-  <textarea rows="3" placeholder="Escribe una descripción" />
-  <button>Guardar</button>
-</form>
+<section>
+  <form on:submit|preventDefault={handleSubmit}>
+    <input type="text" placeholder="Escribe una tarea" />
+    <textarea rows="3" placeholder="Escribe una descripción" />
+    <button>Guardar</button>
+  </form>
+</section>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  section {
+    max-width: 80%;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    background: rgb(255, 255, 255);
+    min-height: 350px;
+    width: 90%;
+    border-radius: 20px;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 0 30px rgba(255, 55, 255, 0.5);
+  }
+
+  input,
+  textarea {
+    max-width: 90%;
+    width: 100%;
+    height: 30px;
+    margin: 20px;
+    padding-left: 10px;
+  }
+
+  textarea {
+    height: 80px;
+    line-height: 18px;
+  }
+
+  button {
+    width: 150px;
+    border-radius: 20px;
+    border: none;
+    background: #09f;
+    color: white;
+    font-size: 20px;
+    height: 40px;
+  }
 </style>
