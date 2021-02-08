763e9e5955c866c71d8aa099de202a11531f029d
Ramiro Garcia <rg2607836@gmail.com>
2021-02-08 19:16:08 -0300

    [Add] haciendo el form para agregar las tareas

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,15 @@
 <script>
+  const handleSubmit = (e) => {
+    e.preventDefault("");
+    console.log("Guardado");
+  };
 </script>
 
+<form on:submit={handleSubmit}>
+  <input type="text" placeholder="Escribe una tarea" />
+  <textarea rows="3" placeholder="Escribe una descripción" />
+  <button>Guardar</button>
+</form>
+
 <style>
-	
 </style>
